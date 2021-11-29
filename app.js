f092699aa224a68719745e6e1a9ab02d028ced99
Anders N Kodehode <anders.kodehode@gmail.com>
2021-11-29 09:41:26 +0100

    changed background

--- app.js
+++ app.js
@@ -16,6 +16,30 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
+// Pokeball
+const ball = document.querySelector('.ball');
+const innerButton = document.querySelector('.inner-button');
+
+ball.addEventListener('click', function(e) {
+    e.preventDefault;
+
+    ball.classList.remove('ball');
+
+    void ball.offsetWidth;
+
+    ball.classList.add('ball');
+}, false);
+
+innerButton.addEventListener('click', function(e) {
+    e.preventDefault;
+
+    innerButton.classList.remove('inner-button');
+
+    void innerButton.offsetWidth;
+
+    innerButton.classList.add('inner-button');
+}, false);
+
 // Calculator
 const display = document.querySelector('.result-screen')
 const buttons = Array.from(document.querySelectorAll('button'))
